0c55df6a40764123190d380013e16729aea6686d
Andy <martinyukaa@ukr.net>
2024-04-21 16:30:07 +0200

    TESO Fisher UI 2.0 - ESOLocateCapture

---
+++ screenCapture/eso_locate_capture.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+from luaParser.eso_locate_parser import ESOLocateParser
+from screenCapture.screen_capture import ScreeCapture
+
+
+class ESOLocateCapture(ScreeCapture):
+    main_color = np.array([207, 220, 189])
+
+    img: Image
+
+    @classmethod
+    def get_cap(cls, **kwargs):
+        super().get_cap(
+            point_left=ESOLocateParser.left_point,
+            point_top=ESOLocateParser.top_point,
+            point_right=ESOLocateParser.right_point,
+            point_bottom=ESOLocateParser.bottom_point)
+
+        cls.capture = cls.capture[:, 100:200, :]
+
+    @classmethod
+    def convert_image_2_text(cls):
+        cls.img = cls.resize_xn(
+            Image.fromarray(
+                obj=cls.capture,
+                mode='RGB'
+            ), [3, 3]
+        )
+        # .img.filter(ImageFilter.BoxBlur(200))
+        # cls.img = ImageEnhance.Sharpness(cls.img).enhance(0.01)
+
+        for _ in range(1):
+            cls.img = cls.img.filter(ImageFilter.MinFilter(3))
+        return pytesseract.image_to_string(cls.img, config='r-l equ')
---
+++ screenCapture/screen_capture.py
@@ -0,0 +1,44 @@
+"""Scree capture"""
+from typing import Any
+
+import numpy as np
+from PIL import ImageGrab, Image
+from numpy import ndarray, dtype
+
+
+class ScreeCapture:
+    capture: ndarray[Any, dtype[Any]]
+
+    main_color: ndarray[Any, dtype[Any]]
+    white_color = np.array([225, 225, 225])
+    black_color = np.array([0, 0, 0])
+
+    @classmethod
+    def get_cap(cls, point_left: int, point_top: int, point_right: int, point_bottom: int) -> None:
+        """Captures the screen and returns the image obtained from the left-top and right-bottom points"""
+        cls.capture = np.array(
+            ImageGrab.grab(
+                bbox=(point_left, point_top, point_right, point_bottom)
+            )
+        )
+
+        cls.segmentation()
+
+    @classmethod
+    def resize_xn(cls, img: Image, xn: int | list[int]) -> Image:
+        if isinstance(xn, int):
+            width = xn
+            height = xn
+        else:
+            width = xn[0]
+            height = xn[1]
+        return img.resize((img.size[0] * width, img.size[1] * height))
+
+    @classmethod
+    def segmentation(cls):
+        for line in range(cls.capture.shape[0]):
+            for colum in range(cls.capture.shape[1]):
+                if not np.array_equal(cls.capture[line][colum], cls.main_color):
+                    cls.capture[line, colum] = cls.white_color
+                else:
+                    cls.capture[line, colum] = cls.black_color
