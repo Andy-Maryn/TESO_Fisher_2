69a22cd25f4d0bd8f48eda64503718cd39d77290
Andy <martinyukaa@ukr.net>
2024-04-26 20:42:48 +0200

    TESO Fisher UI 2.0 - YetAnotherCompassCapture: get_compas_degree -> get_compas_direction

--- screenCapture/yet_another_compass_capture.py
+++ screenCapture/yet_another_compass_capture.py
@@ -3,8 +3,7 @@ from typing import Callable
 import numpy as np
 from PIL import Image, ImageFilter
 
-from luaParser.yet_another_compass_parser import YetAnotherCompassParser as yacp
-from moving.moving import Moving
+from luaParser.yet_another_compass_parser import YetAnotherCompassParser
 from screenCapture.screen_capture import ScreeCapture
 
 
@@ -14,33 +13,34 @@ class YetAnotherCompassCapture(ScreeCapture):
     @classmethod
     def get_cap(cls, **kwargs):
         super().get_cap(
-            point_left=yacp.left_point,
-            point_top=yacp.top_point,
-            point_right=yacp.right_point,
-            point_bottom=yacp.bottom_point)
+            point_left=YetAnotherCompassParser.left_point,
+            point_top=YetAnotherCompassParser.top_point,
+            point_right=YetAnotherCompassParser.right_point,
+            point_bottom=YetAnotherCompassParser.bottom_point)
 
     @classmethod
     def get_tip(cls) -> tuple[int, int]:
         cls.capture = Image.fromarray(cls.capture).filter(ImageFilter.MaxFilter(3))
         cls.capture = np.asarray(cls.capture)
+        
+        _size = YetAnotherCompassParser.size
+        _to = _size // 3
+        _from = _size - _to
 
-        _to = yacp.size // 3
-        _from = yacp.size - (yacp.size // 3)
-
-        direction_minor = range(0, yacp.size)
+        direction_minor = range(0, _size)
 
         direction_major: range
-        forward_direction = range(0, yacp.size, 1)
-        inverse_direction = range(yacp.size - 1, 0, -1)  # for bottom
+        forward_direction = range(0, _size, 1)
+        inverse_direction = range(_size - 1, 0, -1)  # for bottom
 
         direction_axes: Callable
         direction_axes_x: Callable = lambda x, i, j: (x[i, j, 0], i, j)
         direction_axes_y: Callable = lambda x, i, j: (x[j, i, 0], j, i)
 
-        left = ((yacp.size // 3) * yacp.size) - np.sum(cls.capture[:, :_to, 0] // 255)
-        right = ((yacp.size // 3) * yacp.size) - np.sum(cls.capture[:, _from:, 0] // 255)
-        top = ((yacp.size // 3) * yacp.size) - np.sum(cls.capture[:_to, :, 0] // 255)
-        bottom = ((yacp.size // 3) * yacp.size) - np.sum(cls.capture[_from:, :, 0] // 255)
+        left = (_to * _size) - np.sum(cls.capture[:, :_to, 0] // 255)
+        right = (_to * _size) - np.sum(cls.capture[:, _from:, 0] // 255)
+        top = (_to * _size) - np.sum(cls.capture[:_to, :, 0] // 255)
+        bottom = (_to * _size) - np.sum(cls.capture[_from:, :, 0] // 255)
 
         current_side = max(left, right, top, bottom)
 
@@ -71,15 +71,15 @@ class YetAnotherCompassCapture(ScreeCapture):
         return find_point(direction_major, direction_axes)
 
     @classmethod
-    def get_compas_degree(cls) -> float | None:
+    def get_compas_direction(cls) -> tuple[float, float] | None:
         """
           0,   0 -> -75,  75
         150, 150 ->  75, -75
         :return:
         """
-        mid_point = yacp.size // 2
-        x_axes = lambda x: x - mid_point
-        y_axes = lambda x: mid_point - x
+        mid_point = YetAnotherCompassParser.size // 2
+        x_axes: Callable[[int], int] = lambda _x: _x - mid_point
+        y_axes: Callable[[int], int] = lambda _y: mid_point - _y
 
         tip = cls.get_tip()
 
@@ -87,8 +87,6 @@ class YetAnotherCompassCapture(ScreeCapture):
             y, x = tip
             x = x_axes(x)
             y = y_axes(y)
-
-            degree = Moving.get_degree((0, 0), (x, y))
-            return degree
+            return x_axes(x), y_axes(y)
 
         return None
