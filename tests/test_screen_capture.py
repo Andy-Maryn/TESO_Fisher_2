b091f85f76f4fa9cdee1610e239509957603996f
Andy <martinyukaa@ukr.net>
2024-04-30 23:10:28 +0200

    TESO Fisher UI 2.0 - Test: TestESOLocateCapture, TestYetAnotherCompassCapture: added test id

--- tests/test_screen_capture.py
+++ tests/test_screen_capture.py
@@ -4,11 +4,13 @@ from tests.common import *
 class TestESOLocateCapture:
     @pytest.mark.parametrize(
         'eso_locate_capture, expected_location', [
-            pytest.param(Path("4715_8270.jpeg"), [47.15, 82.70]),
-            pytest.param(Path("4719_8287.jpeg"), [47.19, 82.87]),
+            pytest.param(Path("4715_8270.jpeg"), [47.15, 82.70], id="4715_8270.jpeg => position: [47.15, 82.70]"),
+            pytest.param(Path("4719_8287.jpeg"), [47.19, 82.87], id="4719_8287.jpeg => position: [[47.19, 82.87]"),
         ], indirect=['eso_locate_capture']
     )
-    def test_eso_locate_get_current_position(self, eso_locate_capture, expected_location):
+    def test_eso_locate_get_current_position(self,
+                                             eso_locate_capture: None,
+                                             expected_location: tuple[float, float]):
         actual_position = ESOLocateCapture.get_current_position()
         assert actual_position == expected_location
 
@@ -16,12 +18,12 @@ class TestESOLocateCapture:
 class TestYetAnotherCompassCapture:
     @pytest.mark.parametrize(
         'yet_another_compass_capture, expected_location', [
-            pytest.param(Path("4730_8228.jpeg"), (-21, 55)),
-            pytest.param(Path("4739_8325.jpeg"), (32, 39)),
-            pytest.param(Path("4777_8315.jpeg"), (49, -18)),
-        ], indirect=['yet_another_compass_capture']
-    )
-    def test_eso_locate_get_compas_direction(self, yet_another_compass_capture, expected_location):
-
+            pytest.param(Path("4730_8228.jpeg"), (-21, 55), id="4730_8228.jpeg => tip: (-21, 55)"),
+            pytest.param(Path("4739_8325.jpeg"), (32, 39), id="4739_8325.jpeg => tip: (32, 39)"),
+            pytest.param(Path("4777_8315.jpeg"), (49, -18), id="4777_8315.jpeg => tip: (49, -18)"),
+        ], indirect=['yet_another_compass_capture'])
+    def test_eso_locate_get_compas_direction(self,
+                                             yet_another_compass_capture: None,
+                                             expected_location: tuple[float, float]):
         actual_position = YetAnotherCompassCapture.get_compas_direction()
         assert actual_position == expected_location
