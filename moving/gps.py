d6f3885f28311cad3a17c8d886ff6dd9b8b8d51a
Andy <martinyukaa@ukr.net>
2024-05-02 23:44:40 +0200

    TESO Fisher UI 2.0 - Gps: verify_current_position, verify_current_position

--- moving/gps.py
+++ moving/gps.py
@@ -7,9 +7,10 @@ from screenCapture.eso_locate_capture import ESOLocateCapture
 
 
 class Gps:
+    current_position: tuple[float, float]
 
     @classmethod
-    def get_current_position(cls, wait: int = 5):
+    def get_current_position(cls, wait: int = 5) -> tuple[float, float]:
         start_time = time.time()
 
         def get_position(current_time: float = time.time()):
@@ -24,11 +25,26 @@ class Gps:
             else:
                 return None
 
-        return get_position()
+        cls.current_position = get_position()
+
+        return cls.current_position
 
     @classmethod
-    def step_get_degree_between_current_and_destination_points(cls):
-        pass
+    def verify_current_position(cls, start_point: tuple[float, float], destination_point: tuple[float, float]):
+        verify = [False, False]
+        for i in range(2):
+            bord_value = [start_point[i], destination_point[i]]
+            if min(bord_value) - 1 <= cls.current_position[i] <= max(bord_value) + 1:
+                verify[i] = True
+        return all(verify) is True
+
+    @classmethod
+    def is_it_destination_point(cls, destination_point: tuple[float, float]):
+        verify = [False, False]
+        for i in range(2):
+            if destination_point[i] - 1 <= cls.current_position[i] <= destination_point[i] + 1:
+                verify[i] = True
+        return all(verify) is True
 
 
 if __name__ == "__main__":
